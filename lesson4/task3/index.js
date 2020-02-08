c259480cd652312ce2e7afa1c512129126c7df5b
Nadyazns <nadya_zns@yahoo.com>
2020-02-08 12:03:48 +0200

    ok

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 5; i++) {
-    for (n = 1; n <= 10; n++) {
+    for (let n = 1; n <= 10; n++) {
         console.log(i + ' x ' + n + ' = ' + i * n);
     }
-    console.log('=> ' + i + ' <=')
+    console.log('=> ' + i + ' <=');
 }
