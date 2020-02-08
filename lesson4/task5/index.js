2c9d6b9c9c4faf0516d895fbd179a3e03049f417
Nadyazns <nadya_zns@yahoo.com>
2020-02-08 18:11:52 +0200

    done

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,10 +1,12 @@
 const m = 5;
 let i = m;
-const n = 17;
+const n = 25;
 let result = 1;
 
-for (i = 5; i <= n; i++) {
-    if (i % 2 === 0 && i % 4 != 0) {
+while (i <= n && i >= m) {
+    if (i % 5 == 0) {
+        console.log(i)
+    } else if (i % 2 == 0 && i % 4 != 0) {
         result += i;
 
     } else if (i % 3 == 0) {
@@ -13,7 +15,7 @@ for (i = 5; i <= n; i++) {
     } else if (i % 4 == 0) {
         result *= i;
 
-    } else if (i % 5 == 0) {
-        console.log(i)
     }
+    i++;
+    continue;
 }
