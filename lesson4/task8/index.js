7ac0fb9cb71e3402925c8d7d6fc6041eb1432d4a
Nadyazns <nadya_zns@yahoo.com>
2020-02-09 13:03:44 +0200

    ok

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -2,5 +2,6 @@ let sum = 0;
 
 for (i = 1; i <= 1000; i++) {
     sum += i;
+    console.log(sum);
 }
-(Math.floor(sum / 1234) > (sum % 1234)) ? console.log('true'): console.log('false')
+(Math.floor(sum / 1234) > (sum % 1234)) ? console.log('true'): console.log('false');
