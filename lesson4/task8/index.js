d7619837802b6c80ff4bedb439f6c84e9b990d94
Nadyazns <nadya_zns@yahoo.com>
2020-02-09 13:12:42 +0200

    bool

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -2,5 +2,6 @@ let sum = 0;
 
 for (i = 1; i <= 1000; i++) {
     sum += i;
+
 }
-(Math.floor(sum / 1234) > (sum % 1234)) ? console.log('true'): console.log('false');
+console.log(Boolean(Math.floor(sum / 1234) > (sum % 1234))
