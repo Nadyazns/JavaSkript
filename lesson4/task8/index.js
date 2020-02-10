1989c1ee831e1fba674f85828d48617e280eda61
Nadyazns <nadya_zns@yahoo.com>
2020-02-10 14:28:05 +0200

    fix

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -3,5 +3,7 @@ let sum = 0;
 for (i = 0; i <= 1000; i++) {
     sum += i;
 }
-
-console.log(Boolean((sum / 1234) > (sum % 1234)));
+console.log(sum)
+let a = sum / 1234;
+let b = sum % 1234;
+console.log(Boolean(a > b));
