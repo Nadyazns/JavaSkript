d685ddb7a9504f37ac12820682393b362e62a6d0
Nadyazns <nadya_zns@yahoo.com>
2020-02-11 23:55:27 +0200

    ok

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,7 +1,11 @@
 function getPrimes(num) {
     for (let f = 2; f <= num; f++) {
-        for (let i = 2; i < num; i++)
-            if (num % i === 0) return false;
-        return console.log(f);
+        let i;
+        for (i = 2; i < f; i++)
+            if (f % i === 0) {
+                return false;
+            }
+        console.log(i);
+
     }
 }
---
+++ lesson5/task8/index.js
@@ -0,0 +1,12 @@
+function sum(from, to) {
+    let total = 0;
+    for (let a = from; a <= to; a++) {
+        total += a;
+    }
+    console.log(total)
+}
+
+
+function compareSums(a, b, c, d) {
+    return Boolean(sum(a, b) > sum(c, d))
+}
