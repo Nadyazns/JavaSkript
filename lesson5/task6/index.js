6612f69e0e02cf3e9a59067eb107f59ed776a7bd
Nadyazns <nadya_zns@yahoo.com>
2020-02-12 13:54:20 +0200

    done

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,11 +1,14 @@
-function getPrimes(num) {
-    for (let f = 2; f <= num; f++) {
-        let i;
-        for (i = 2; i < f; i++)
-            if (f % i === 0) {
-                return false;
-            }
-        console.log(i);
+function isPrime(number) {
+    for (let i = 2; i < number; i++)
+        if (number % i === 0)
+            return false;
+    return true;
+}
 
+function getPrimes(num) {
+    for (let f = 1; f <= num; f++) {
+        if (isPrime(f)) {
+            console.log(f)
+        }
     }
 }
