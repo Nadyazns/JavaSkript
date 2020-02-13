da92e51b286ff5c509b2fc526f50100a81f58b9a
Nadyazns <nadya_zns@yahoo.com>
2020-02-13 12:00:20 +0200

    ok

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -2,7 +2,7 @@
 // const arrNa = new Array (1,2,3) - функция конструктор
 // new Array () - пустой массив
 // new Array (5) - массив из 5 элементов
-const arrayName = [1, 2, 3]
+const arrayName = [1, 2, 3, 9]
 
 
 function getSum(arrayName) {
@@ -10,7 +10,7 @@ function getSum(arrayName) {
     if (!Array.isArray) {
         return null;
     }
-    for (i = 0; i < arrayName.length; i++) {
+    for (let i = 0; i < arrayName.length; i++) {
         sum += arrayName[i];
     }
     return sum;
