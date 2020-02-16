daeba85bfbab64bda8c4621e7588fb8b19e933a8
Nadyazns <nadya_zns@yahoo.com>
2020-02-16 20:04:23 +0200

    ok

--- lesson6/task11/index.js
+++ lesson6/task11/index.js
@@ -6,11 +6,11 @@ function checker(arr) {
     let min = arr[0];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > max) max = arr[i];
-        console.log(max);
+
     }
     for (i = 0; i < arr.length; i++) {
         if (arr[i] < min) min = arr[i];
-        console.log(min);
+
     }
     if (min + max < 1000)
         return false;
