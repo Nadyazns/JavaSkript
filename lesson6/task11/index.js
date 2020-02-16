8c28e67aa4654c13b77af82406db3abfdd914f8c
Nadyazns <nadya_zns@yahoo.com>
2020-02-16 19:59:08 +0200

    ok

---
+++ lesson6/task11/index.js
@@ -0,0 +1,19 @@
+//let balances = [23, 245, 120, 25, 1];
+
+function checker(arr) {
+    let sum = 0;
+    let max = arr[0];
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+        console.log(max);
+    }
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+        console.log(min);
+    }
+    if (min + max < 1000)
+        return false;
+    return true;
+}
+//console.log(checker(balances))
