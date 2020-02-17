272a231edb9c3e19f65f54f4ea3783a6240dbe58
Nadyazns <nadya_zns@yahoo.com>
2020-02-17 14:15:41 +0200

    ok

--- lesson6/task12/index.js
+++ lesson6/task12/index.js
@@ -1,7 +1,10 @@
 const myArray = [1, 67, 45, 23, 7, 34, 68, 2444];
 
 function cloneArr(arr) {
-    let clone = arr;
+    let clone = [];
+    for (var i in arr) {
+        clone[i] = arr[i];
+    }
     return clone;
 }
 
