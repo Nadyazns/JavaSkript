435f0ff71609d95ee0cffa1d97576006646f3552
Nadyazns <nadya_zns@yahoo.com>
2020-02-17 19:52:00 +0200

    done

--- lesson6/task10/index.js
+++ lesson6/task10/index.js
@@ -16,6 +16,20 @@ function withdraw(clients, balances, client, amount) {
 
 }
 
+/*let index = 0;
+for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) {
+            index=[i];
+            break;
+        }
+    }
+    if(balances[index]>=amount){
+        balances[index]-=amount;
+        return balances[index];
+    }
+    return -1*/
+
+
 
 
 
