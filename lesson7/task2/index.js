a81c89c235dd9c00c7d38ca8ef160551cd65f7d6
Nadyazns <nadya_zns@yahoo.com>
2020-02-20 16:52:19 +0200

    done

---
+++ lesson7/task2/index.js
@@ -0,0 +1,26 @@
+// unsorted array => sorted array
+
+
+const sortDesc = numbers => {
+    const copy = numbers.slice();
+    numbers.sort((a, b) => b - a);
+    return copy;
+}
+
+
+
+
+// const sortArray = numbers => {
+//     function compare(a, b) {
+//         if (a > b) {
+//             return 1;
+//         }
+//         return -1;
+//     }
+//     numbers.sort(compare);
+//     return numbers;
+// };
+
+// const arr = [4, 92387, 2727, 1, 0, 377, 12]
+// console.log(sortDesc(arr));
+// console.log(arr)
