128f37173c40b9d8417dc3ff38ea51e6626e9787
Nadyazns <nadya_zns@yahoo.com>
2020-02-23 10:28:44 +0200

    done

--- lesson7/task6/index.js
+++ lesson7/task6/index.js
@@ -2,6 +2,9 @@ function reverseArray(arr) {
     if (!Array.isArray) {
         return null;
     }
-    const clone = []
+    return arr.slice().reverse();
 
 }
+
+// const arr = [34, 6, 2, 7, 99, 0, 142];
+// console.log(reverseArray(arr))
