03d3d1861e5f67578805088401289c2b3e60b117
Nadyazns <nadya_zns@yahoo.com>
2020-05-12 20:36:13 +0300

    ok

--- lesson6/task14/index.js
+++ lesson6/task14/index.js
@@ -1,14 +1,14 @@
-arr = [12, 7, 45, 99]
+// arr = [12, 7, 45, 99]
 
 
 function getSubArray(arr, num) {
     let subArray = []
     for (let i = 0; i <= num - 1; i++) {
         subArray.unshift(arr[i]);
-        console.log(subArray)
+
     }
 
     return subArray;
 }
 
-getSubArray(arr, 2);
+// getSubArray(arr, 2);
