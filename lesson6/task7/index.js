1b5d6e52eba18e01fe7d1f21876cb7aef24c72b7
Nadyazns <nadya_zns@yahoo.com>
2020-05-12 18:46:54 +0300

    ok

--- lesson6/task7/index.js
+++ lesson6/task7/index.js
@@ -1,17 +1,17 @@
-let arr = [10, 20, 100]
-let sumOfElements = 0;
+let arr = [10, 20, 10]
+
 
 function checkSum(arr) {
+    let sumOfElements = 0;
     if (!Array.isArray(arr)) {
         return null;
-    } else {
-        for (let i = 0; i <= arr.length; i++) {
-            sumOfElements += arr[i];
-            if (sumOfElements > 100) {
-                return true;
-            }
-        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        sumOfElements += arr[i];
 
+    }
+    if (sumOfElements > 100) {
+        return true;
     }
     return false;
 }
