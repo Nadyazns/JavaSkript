f8e531e4ba860e7efa18bcd1d4c07ba466bf4f69
Nadyazns <nadya_zns@yahoo.com>
2020-05-14 15:26:06 +0300

    k

--- lesson7/task8/index.js
+++ lesson7/task8/index.js
@@ -3,9 +3,11 @@
 function cloneArr(arr) {
     if (!Array.isArray(arr)) {
         return null;
+    } else {
+        let clone = [...rest] = arr;
+        return clone;
     }
-    let clone = [...rest] = arr;
-    return clone;
+
 }
 
 
