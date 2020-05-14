20f44d7caaf41875e20c0d7785c867c36c052866
Nadyazns <nadya_zns@yahoo.com>
2020-05-14 18:21:36 +0300

    ok

---
+++ lesson7/task10/index.js
@@ -0,0 +1,14 @@
+// const arr = [1, 2, 4, 5];
+
+function sum(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    return arr.reduce(function(acc, num) {
+        return acc + num;
+    });
+}
+
+
+// console.log(arr);
+// console.log(sum(arr));
