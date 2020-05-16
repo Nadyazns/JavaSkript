68997b51a03ca413e5a73886ebd32202936543c6
Nadyazns <nadya_zns@yahoo.com>
2020-05-16 17:45:13 +0300

    ok

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,25 +1,28 @@
 // input: Array;
-// output: Array;
+// output: number;
 
 // algo:
-// 1. find clients and index of client;
-// 2. find clients balance;
-// 3. withraw money from clients balance;
-// 4. return new Array;
+// 1. find index of the client;
+// 2. find  clints balance and compare with amount;
+// 3. withraw money;
+// 4. return newBalance;
 
 
 // let clients = ['Alex', 'Nadya', 'Ann', 'Tolik'];
 // let balances = [200, 500, 800, 450];
 
 let withdraw = (clients, balances, client, amount) => {
-    let indexOfElem = balances[clients.indexOf(client)] = balances[clients.indexOf(client)] - amount;
+    let neededIndex = clients.indexOf(client);
 
-    if (amount > balances[clients.indexOf(client)]) {
-        delete balances[indexOfElem];
+    let newBalances = [...balances];
+    if ((newBalances[neededIndex] = newBalances[neededIndex] - amount) < 0) {
+
         return -1;
-    }
-    return balances;
+    } else {
+        let newBalance = balances[neededIndex] = balances[neededIndex] - amount;
 
+        return newBalance;
+    }
 }
 
-// console.log(withdraw(clients, balances, 'Alex', 500));
+// console.log(withdraw(clients, balances, 'Nadya', 5));
