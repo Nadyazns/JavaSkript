de093a70ccdb29879cf392a5f7c1912e95e079f1
Nadyazns <nadya_zns@yahoo.com>
2020-05-16 19:34:05 +0300

    ok

--- lesson8/task3/index.js
+++ lesson8/task3/index.js
@@ -1,41 +1,24 @@
-// const obj = {
-//     name: 'Nadya',
-//     sex: 'Female',
-//     'fav color': 'Brown',
-//     music: 'Soul',
-//     age: 32,
-// }
+const transaction = {
+    currency: 'USD',
+    value: 170,
+    operation: 'sale',
+    agent: {
+        country: 'Ukraine',
+        company: 'NYSE'
+    },
+    'operation time': 1584029990001
+};
 
-// const arr = ['name', "age"];
-
-
-const pickProps = (obj, arr) => {
-    let myObj = {};
-    arr.forEach(a => {
-        if (obj[a]) {
-            myObj[a] = obj[a];
-        };
-    });
-    return myObj;
-}
-
-//console.log(pickProps(obj, arr));
-
-
-// const baseObject = { xml: 4, http: 2, rest: 2, js: 10 };
-// const baseFields = ["xml", "js"];
-
-// function mapObject(baseObject, baseFields) {
-//     let obj = {};
-//     baseFields.forEach(elem => {
-//         if (baseObject[elem]) {
-//             obj[elem] = baseObject[elem];
-//         }
-//     });
-//     return obj;
-
-//     // или тут console.log(obj); вместо return
-//     // и тогда запускаешь функцию mapObject(baseObject, baseFields)
-//     // без console.log
-// }
-// console.log(mapObject(baseObject, baseFields));
+/* выведите в консоль значение свойства 'currency' с помощью точки */
+console.log(transaction.currency);
+/* выведите в консоль значение свойства 'value' с помощью квадратных скобок */
+console.log(transaction["value"]);
+const key = 'operation';
+/* выведите в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
+console.log(transaction[key]);
+/* выведите в консоль значение свойства 'company' используя квадратные скобки */
+console.log(transaction.agent["company"]);
+/* выведите в консоль значение свойства 'country' используя точку */
+console.log(transaction.agent.country);
+/* выведите в консоль значение свойства 'operation time' используя квадратные скобки */
+console.log(transaction['operation time']);
