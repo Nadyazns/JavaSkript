25296d980843b4833fc3a94cd66738cb5cf51c32
Nadyazns <nadya_zns@yahoo.com>
2020-05-17 23:59:42 +0300

    ok

---
+++ lesson8/task7/index.js
@@ -0,0 +1,45 @@
+let userData = {
+    name: 'Nadya',
+    age: 33,
+    country: 'Ukraine',
+}
+
+const addProperty1 = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
+}
+
+const a = addProperty1(userData, "sex", 'female');
+console.log(a)
+
+const addProperty2 = (obj, key, value) => {
+    let a = {};
+    a[key] = value;
+    Object.assign(obj, a);
+    return obj;
+}
+
+const b = addProperty2(userData, "sex", 'female');
+console.log(b);
+
+const addProperty3 = (obj, key, value) => {
+    let a = {};
+    a[key] = value;
+    Object.assign(obj, a);
+
+
+    return obj;
+}
+
+const c = addProperty3(userData, "sex", 'female');
+console.log(c);
+
+const addProperty4 = (obj, key, value) => {
+    let a = {...obj };
+    a[key] = value;
+    Object.assign(obj, a);
+    return a;
+}
+
+const d = addProperty4(userData, "sex", 'female');
+console.log(d);
