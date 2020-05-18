2011ae9afd7cdc46e0f08a5b5630e76d2f464bd0
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 10:22:10 +0300

    ok

--- standups/functions/task3/index.js
+++ standups/functions/task3/index.js
@@ -5,7 +5,7 @@
 //output: array of numbers;
 
 
-function squareArray1(arr) {
+function squareArray(arr) {
     if (!Array.isArray(arr))
         return null;
 
@@ -17,26 +17,26 @@ function squareArray1(arr) {
     return result;
 }
 
-function squareArray2(arr) {
-    if (!Array.isArray(arr))
-        return null;
+// function squareArray2(arr) {
+//     if (!Array.isArray(arr))
+//         return null;
 
-    let result = [...arr];
-    for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[i] * arr[i];
-        //result[i]=arr[i]*arr[i];
-    }
-    return result;
-}
+//     let result = [...arr];
+//     for (let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i] * arr[i];
+//         //result[i]=arr[i]*arr[i];
+//     }
+//     return result;
+// }
 
-function squareArray3(arr) {
-    if (!Array.isArray(arr))
-        return null;
-    let result = [];
+// function squareArray3(arr) {
+//     if (!Array.isArray(arr))
+//         return null;
+//     let result = [];
 
-    for (let el of arr) {
-        result.push(el * el);
-        //result[i]=arr[i]*arr[i];
-    }
-    return result;
-}
+//     for (let el of arr) {
+//         result.push(el * el);
+//         //result[i]=arr[i]*arr[i];
+//     }
+//     return result;
+// }
