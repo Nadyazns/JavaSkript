7c7ef0ef16215b2c4df8d46a79e6cc9dadcb4522
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 18:31:05 +0300

    ok

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -1,50 +1,67 @@
-// let obj1 = {
-//     name: 'Nadya',
-//     age: 33,
-//     country: 'Ukraine',
-// }
-// let obj2 = {
-//     hair: 'red',
-//     age: 18,
-// }
-// let obj3 = {
-//     name: 'Nadya',
-//     age: 33,
-//     country: 'Ukraine',
-// }
-// let obj4 = {
-//     hair: 'red',
-//     age: 18,
-//     sex: 'female',
-// }
+let obj1 = {
+    name: 'Nadya',
+    age: 33,
+    country: 'Ukraine',
+}
+let obj2 = {
+    hair: 'red',
+    age: 18,
+}
+let obj3 = {
+    name: 'Nadya',
+    age: 33,
+    country: 'Ukraine',
+}
+let obj4 = {
+    hair: 'red',
+    age: 18,
+    sex: 'female',
+}
 
-// let obj5 = {
-//     name: 'Nadya',
-//     age: 33,
-//     home: 'Ukraine',
-// }
-// let obj6 = {
-//     name: 'Nadya',
-//     age: 33,
-//     home: 'donetsk',
-// }
+let obj5 = {
+    name: 'Nadya',
+    age: 33,
+    home: 'Ukraine',
+}
+let obj6 = {
+    name: 'Nadya',
+    age: 33,
+    home: 'donetsk',
+}
 
-const compareObjects = (obj1, obj2) => {
-        for (let key in obj1) {
-            for (let key1 in obj2) {
-                if (obj1[key] != obj2[key] && key != key1) {
-                    // console.log(obj1[key] + ' and ' + obj2[key]);
-                    // console.log(key + ' and ' + key1);
-                    return false;
+// algorythm:
+// 1. write a function to transform keys and values into array;
+// 2 check if the length of the objects is the same
+// 3  compare values;
+// 4 if true return true.
+// 5 if false return false.
 
-                }
 
+const compareObjects = (a, b) => {
+    let keys1 = Object.keys(a);
+    let keys2 = Object.keys(b);
+    let val1 = Object.values(a);
+    let val2 = Object.values(b);
+
+    if (keys1.length != keys2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < keys1.length; i++) {
+            if (keys1[i] != keys2[i]) {
+                return false;
             }
-
+        }
+    }
+    for (let d = 0; d < val1.length; d++) {
+        if (val1[d] != val2[d]) {
+            return false;
 
         }
-
-        return true;
     }
-    // let result = compareObjects(obj5, obj6);
-    // console.log(result)
+    return true;
+}
+
+
+
+let result = compareObjects(obj1, obj2);
+console.log(result);
