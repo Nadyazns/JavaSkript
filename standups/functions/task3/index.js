eb9d9ab66cab742ff28dbd1be9846e8b5c2d6014
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 10:20:16 +0300

    ok

---
+++ standups/functions/task3/index.js
@@ -0,0 +1,42 @@
+// let array1 = [2, 4, 6, 7];
+
+// console.log(array1[array1.length - 2]);
+//inout: array of numbers;
+//output: array of numbers;
+
+
+function squareArray1(arr) {
+    if (!Array.isArray(arr))
+        return null;
+
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i] * arr[i]);
+        //result[i]=arr[i]*arr[i];
+    }
+    return result;
+}
+
+function squareArray2(arr) {
+    if (!Array.isArray(arr))
+        return null;
+
+    let result = [...arr];
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i] * arr[i];
+        //result[i]=arr[i]*arr[i];
+    }
+    return result;
+}
+
+function squareArray3(arr) {
+    if (!Array.isArray(arr))
+        return null;
+    let result = [];
+
+    for (let el of arr) {
+        result.push(el * el);
+        //result[i]=arr[i]*arr[i];
+    }
+    return result;
+}
