9cdbceb90fe62c1f0c39958a4e99bbd930a5e66c
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 12:30:19 +0300

    ok

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -26,8 +26,9 @@ const compareObjects = (obj1, obj2) => {
             if (obj1[key] == obj2[key1] && key === key1) {
                 return true;
             }
-            return false;
+
         }
+        return false;
     }
 
 }
