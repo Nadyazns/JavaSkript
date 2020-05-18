85cca6ee591a250a08bd174d5cad87ebd1fc1a11
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 14:36:38 +0300

    ok

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -1,50 +1,50 @@
-let obj1 = {
-    name: 'Nadya',
-    age: 33,
-    country: 'Ukraine',
-}
-let obj2 = {
-    hair: 'red',
-    age: 18,
-}
-let obj3 = {
-    name: 'Nadya',
-    age: 33,
-    country: 'Ukraine',
-}
-let obj4 = {
-    hair: 'red',
-    age: 18,
-    sex: 'female',
-}
+// let obj1 = {
+//     name: 'Nadya',
+//     age: 33,
+//     country: 'Ukraine',
+// }
+// let obj2 = {
+//     hair: 'red',
+//     age: 18,
+// }
+// let obj3 = {
+//     name: 'Nadya',
+//     age: 33,
+//     country: 'Ukraine',
+// }
+// let obj4 = {
+//     hair: 'red',
+//     age: 18,
+//     sex: 'female',
+// }
 
-let obj5 = {
-    name: 'Nadya',
-    age: 33,
-    home: 'Ukraine',
-}
-let obj6 = {
-    name: 'Nadya',
-    age: 33,
-    home: 'donetsk',
-}
+// let obj5 = {
+//     name: 'Nadya',
+//     age: 33,
+//     home: 'Ukraine',
+// }
+// let obj6 = {
+//     name: 'Nadya',
+//     age: 33,
+//     home: 'donetsk',
+// }
 
 const compareObjects = (obj1, obj2) => {
-    for (let key in obj1) {
-        for (let key1 in obj2) {
-            if (obj1[key] != obj2[key] && key != key1) {
-                // console.log(obj1[key] + ' and ' + obj2[key]);
-                // console.log(key + ' and ' + key1);
-                return false;
+        for (let key in obj1) {
+            for (let key1 in obj2) {
+                if (obj1[key] != obj2[key] && key != key1) {
+                    // console.log(obj1[key] + ' and ' + obj2[key]);
+                    // console.log(key + ' and ' + key1);
+                    return false;
+
+                }
 
             }
 
+
         }
 
-
+        return true;
     }
-
-    return true;
-}
-let result = compareObjects(obj5, obj6);
-console.log(result)
+    // let result = compareObjects(obj5, obj6);
+    // console.log(result)
