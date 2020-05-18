59d4f9de40f0ba233b3ad083b025cc43ca8c4dd5
Nadyazns <nadya_zns@yahoo.com>
2020-05-18 10:41:39 +0300

    ok

--- lesson8/task7/index.js
+++ lesson8/task7/index.js
@@ -4,42 +4,42 @@ let transaction = {
     country: 'Ukraine',
 }
 
-const addProperty1 = (obj, key, value) => {
+const addPropertyV1 = (obj, key, value) => {
     obj[key] = value;
     return obj;
 }
 
-const result = addProperty1(transaction, 'sex', 'female');
+const result = addPropertyV1(transaction, 'sex', 'female');
 console.log(result)
 
-const addProperty2 = (obj, key, value) => {
+const addPropertyV2 = (obj, key, value) => {
     let a = {};
     a[key] = value;
     Object.assign(obj, a);
     return obj;
 }
 
-const result1 = addProperty2(transaction, 'sex', 'female');
+const result1 = addPropertyV2(transaction, 'sex', 'female');
 console.log(result1);
 
-const addProperty3 = (obj, key, value) => {
+const addPropertyV3 = (obj, key, value) => {
     let a = {};
     a[key] = value;
     Object.assign(obj, a);
     return obj;
 }
 
-const result2 = addProperty3(transaction, 'sex', 'female');
+const result2 = addPropertyV3(transaction, 'sex', 'female');
 console.log(result2);
 
 
 
-const addProperty4 = (obj, key, value) => {
+const addPropertyV4 = (obj, key, value) => {
     let a = {...obj };
     a[key] = value;
     Object.assign(obj, a);
     return a;
 }
 
-const result3 = addProperty4(transaction, 'sex', 'female');
+const result3 = addPropertyV4(transaction, 'sex', 'female');
 console.log(result3);
