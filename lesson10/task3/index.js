ab993bef64aae7ee82b4106ffd523597e4f05dee
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 19:09:47 +0300

    ok

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -2,8 +2,7 @@ const multiRound = num => [
     Math.floor(num),
     Math.round(num),
     Math.ceil(num),
-    Math.trunc(num),
-    num.toFixed(1),
+    Math.trunc(num), +num.toFixed(1),
 ]
 
-console.log(multiRound(17));
+// console.log(multiRound(17));
