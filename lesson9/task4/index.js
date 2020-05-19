eb8afd478ad68867560307bce36f9db51897fcaa
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 17:29:34 +0300

    ok

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -22,25 +22,31 @@ rooms = {
 
 // algo:
 // 1. rooms object transform to Array of its values. array of arrays of objects.
-// 2. from each arrays element (another array) take anothers array element(object) and do its objects.values()
-// 3. push it to the new array
+// 2. make it flat
+// 3. map the name into the new array
 
-const getPeople = rooms => {
+const getPeople = rooms =>
+    peoplesNames = Object.values(rooms).flat().map((obj) => obj.name);
 
-    let peoplesNames = [];
-    let peps = [];
-    let namesListOfArrOfObj = Object.values(rooms); // 1. [[{name:"room1 name1"},{name:"room2 name2"}, {name:"room3 name3"}], [{name:"room1 name1"}, {name: " room2 name2"}], ...]
 
-    let arrOfObj = [];
-    for (let i = 0; i < namesListOfArrOfObj.length; i++) {
-        for (let a = 0; a < namesListOfArrOfObj[i].length; a++) {
-            peps.push(namesListOfArrOfObj[i][a]);
-        }
-    }
-    for (let i = 0; i < peps.length; i++) {
-        peoplesNames.push(peps[i].name);
-    }
-    return peoplesNames;
-}
+// let peoplesNames = [];
+// let namesListOfArrOfObj = Object.values(rooms); // 1. [[{name:"room1 name1"},{name:"room2 name2"}, {name:"room3 name3"}], [{name:"room1 name1"}, {name: " room2 name2"}], ...]
+// let a = namesListOfArrOfObj.flat();
+// peoplesNames = a.map((obj) => obj.name)
+
+
+
+
+// let peps = [];
+// for (let i = 0; i < namesListOfArrOfObj.length; i++) {
+//     for (let a = 0; a < namesListOfArrOfObj[i].length; a++) {
+//         peps.push(namesListOfArrOfObj[i][a]);
+//     }
+// }
+// for (let i = 0; i < peps.length; i++) {
+//     peoplesNames.push(peps[i].name);
+// }
+//     return peoplesNames;
+// }
 
 console.log(getPeople(rooms));
