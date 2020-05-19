3d23b06bdcb5c232b81cd393a96c9bd3d3b9d190
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 17:34:16 +0300

    ok

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -1,18 +1,18 @@
 let rooms = {
     room1: [
         { name: 'room1 name1' },
-        { name: 'room2 name2' },
-        { name: 'room3 name3' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
     ],
     room2: [
-        { name: 'room1 name1' },
+        { name: 'room2 name1' },
         { name: 'room2 name2' },
-        { name: 'room3 name3' },
+        { name: 'room2 name3' },
 
     ],
     room3: [
-        { name: 'room1 name1' },
-        { name: 'room2 name2' },
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
         { name: 'room3 name3' },
     ]
 }
