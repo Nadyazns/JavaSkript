87416912ea9656ea69178edbe5d870b3bf64d1ae
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 14:20:22 +0300

    ok

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -17,26 +17,30 @@ rooms = {
     ]
 }
 
-const getPeople = rooms => {
-    const listOfRooms = Object.entries(rooms);
-    const toArr = listOfRooms.forEach(obj => {
-        for (let key in obj) {
-            Object.values(obj[key])
-        }
-    });
-    console.log(listOfRooms)
+// input: obj;
+// output: array of strings;
 
+// algo:
+// 1. rooms object transform to Array of its values. array of arrays of objects.
+// 2. from each arrays element (another array) take anothers array element(object) and do its objects.values()
+// 3. push it to the new array
+
+const getPeople = rooms => {
+
+    let peoplesNames = [];
+    let peps = [];
+    let namesListOfArrOfObj = Object.values(rooms); // 1. [[{name:"room1 name1"},{name:"room2 name2"}, {name:"room3 name3"}], [{name:"room1 name1"}, {name: " room2 name2"}], ...]
+    console.log(namesListOfArrOfObj);
+    let arrOfObj = [];
+    for (let i = 0; i < namesListOfArrOfObj.length; i++) {
+        for (let a = 0; a < namesListOfArrOfObj[i].length; a++) {
+            peps.push(namesListOfArrOfObj[i][a]);
+        }
+    }
+    for (let i = 0; i < peps.length; i++) {
+        peoplesNames.push(peps[i].name);
+    }
+    return peoplesNames;
 }
 
 console.log(getPeople(rooms))
-
-// const arr = ['as', 1]
-// const transformToObj = arr => {
-//     let obj = {};
-//     arr.forEach(element => {
-//         obj[element] = 23   ;
-
-//     });
-//     return obj;
-// }
-// console.log(transformToObj(arr))
