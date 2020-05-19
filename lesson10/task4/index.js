61d1c63bb2bc26391f9944c68ed63f1759d33be5
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 20:15:32 +0300

    ok

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,7 +1,7 @@
 let numbers = [2, -4, 78, -99]
 
 const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr) || arr == []) {
+    if (!Array.isArray(arr) || arr === []) {
         return null;
     }
     let absoluteNumbers = arr.map(num => Math.abs(num))
