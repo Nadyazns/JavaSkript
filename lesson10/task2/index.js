6d4453c779f1f83b32a7cd5caf9b29934e4b2221
Nadyazns <nadya_zns@yahoo.com>
2020-05-19 18:46:14 +0300

    ok

---
+++ lesson10/task2/index.js
@@ -0,0 +1,13 @@
+let everything = [1, '14', 1.55, 4.72, 8, 99, 'it', {}, NaN]
+
+const getParsedIntegers = arr => arr.map(el => Number.parseInt(el));
+console.log(getParsedIntegers(everything));
+
+const getParsedIntegersV2 = arr => arr.map(el => Number.parseInt(el));
+console.log(getParsedIntegersV2(everything));
+
+const getParsedFloats = arr => arr.map(el => Number.parseFloat(el));
+console.log(getParsedFloats(everything));
+
+const getParsedFloatsV2 = arr => arr.map(el => parseFloat(el));
+console.log(getParsedFloatsV2(everything));
