03acc33033a35ed2b8380872ed5a4a5eaa1a86a3
Nadyazns <nadya_zns@yahoo.com>
2020-05-20 18:33:03 +0300

    ok

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,10 +1,19 @@
+// input:number;
+// output: array;
+
+// algo:
+// 1. function to return array of numbers with given start, end and length;
+// 2. make it random, push into arr.
+
 const getRandomNumbers = (arrlength, start, end) => {
+    if (Math.abs(end - start) < 1)
+        return null;
 
-    if ()
-        let arr = [];
-    for (let i = start; i <= end; i++) {
-        console.log(i);
+    let arrOfRandomNumbers = [];
+    for (let i = 0; i < arrlength; i++) {
+        arrOfRandomNumbers.push(Math.floor(Math.random() * (finish - start) + start));
     }
+    return arrOfRandomNumbers;
 }
-
-console.log(getRandomNumbers(5, 6.75, 12))
+getRandomNumbers(5, 6.75, 12);
+console.log(getRandomNumbers(5, 6.75, 12));
