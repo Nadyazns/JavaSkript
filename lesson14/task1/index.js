af2292d27a936cb82bb64989b7270505ca4ddd64
Nadyazns <nadya_zns@yahoo.com>
2020-05-24 21:31:10 +0300

    ok

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -2,12 +2,11 @@
 
 let message = "Just learn it";
 
-
-
 export const sendMessage = name => {
     const sender = 'Gromcode';
-    console.log(`${name},${message}\! Your ${sender}`)
+    console.log(`${name},${message}! Your ${sender}`)
 };
+
 export function setMessage(text) {
     message = text;
 }
