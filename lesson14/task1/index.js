d85a1cfdf6d46d755be7363675da343b4dba77c3
Nadyazns <nadya_zns@yahoo.com>
2020-05-24 21:11:34 +0300

    ok

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -2,16 +2,16 @@
 'use strict'
 let message = "Just learn it";
 
-export function setMessage(text) {
-    message = text;
-}
+
 
 function sendMessage(name) {
     const sender = 'Gromcode';
     console.log(`${name},${message}! Your ${sender}`)
 };
-
-
+export function setMessage(text) {
+    message = text;
+}
+sendMessage('Ann');
 setMessage('Good job');
 sendMessage('Ann');
 
