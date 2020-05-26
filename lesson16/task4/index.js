56516d21d97ce0ae6ac0013aec3e1e6b44d35059
Nadyazns <nadya_zns@yahoo.com>
2020-05-26 21:13:55 +0300

    ok

---
+++ lesson16/task4/index.js
@@ -0,0 +1,13 @@
+export default foo = 1;
+
+export function bar() {
+
+    if (!foo) {
+
+        let foo = 10;
+    }
+
+    return foo;
+}
+
+var foo = bar();
---
+++ lesson16/task4/index.test.js
@@ -0,0 +1,6 @@
+import variable, { bar } from './index.js';
+
+it('should return foo = 1', () => {
+
+    expect(result).toEqual(1);
+});
