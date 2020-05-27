88d69b99f953d3258ad11e665658f4ed45b784bd
Nadyazns <nadya_zns@yahoo.com>
2020-05-27 17:21:49 +0300

    wrapperfunc

---
+++ lesson17/task3/index.js
@@ -0,0 +1,17 @@
+//defer (func,ms) => Function
+
+export function defer(func, ms) {
+    return function() {
+        setTimeout(() => func.apply(this, arguments), ms)
+    }
+}
+
+// const user = {
+//     name: 'Tom',
+//     sayHi() {
+//         console.log(`Hi! I am ${this.name}!`)
+//     }
+// }
+
+const defferHi = defer(user.sayHi, 1000);
+defferHi.call({ name: "Nadya" });
