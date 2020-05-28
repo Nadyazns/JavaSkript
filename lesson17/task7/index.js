e9ee325b320661c53d271e9cc69ff0a7e1f733e7
Nadyazns <nadya_zns@yahoo.com>
2020-05-28 14:41:35 +0300

    ok

--- lesson17/task7/index.js
+++ lesson17/task7/index.js
@@ -1,13 +1,27 @@
+//input:obj,
+//output: modified obj,
+
+
+// algo
+// 1.getFullName returns full name;
+// 2.setFullName ('name lastname') => modified obj accordingly to name lastname.
+
 const user = {
     firstName: "Nadya",
     lastName: "Zhuravlova",
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
+    },
+    setFullName(name) {
+        let arr = name.split(" ");
+        // console.log(arr[0]);
+        this.firstName = arr[0];
+        this.lastName = arr[1];
+        // console.log(user);
+
     }
 }
 
-
-// const name = user.getFullName();
-function setFullName() {
-
-}
+// console.log(user.getFullName());
+// let name = user.getFullName();
+// console.log(name);
