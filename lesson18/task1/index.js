ea8eaed4346ee3135a7db68edb0faa435f738bf4
Nadyazns <nadya_zns@yahoo.com>
2020-07-05 14:41:55 +0300

    invitation

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -15,12 +15,14 @@ export const event = {
         { name: 'Nadya', email: 'n@gmail.com', age: 33 },
         { name: 'Alex', email: 'a@gmail.com', age: 17 },
     ],
-    message: 'Welcome to the party!',
+    message: ' Welcome to the party!',
     getInvitations() {
-        return this.guests.filter(({ age }) => age >= 18).map(({ name, email }) => ({
-            email,
-            text: `Dear ${name}!${this.message}`
-        }));
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({ name, email }) => ({
+                email,
+                text: `Dear ${name}! ${this.message}`,
+            }));
     }
 }
 
