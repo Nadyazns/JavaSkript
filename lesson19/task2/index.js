09179692dc435f83ccd04cb5fad80a2cbd5a6489
Nadyazns <nadya_zns@yahoo.com>
2020-07-07 15:31:12 +0300

    ownprop

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -5,34 +5,34 @@ export function getOwnProps(obj) {
         }
     }
     // return console.log(Object.keys(obj));
-}
-
-// const vehicle = {
-//     name: 'Argo',
-//     move() {
-//         console.log(`${this.name} is moving`);
-//     },
-//     stop() {
-//         console.log(`${this.name} stopped`);
-//     },
-// }
-
-// // console.log(vehicle);
-
-// const ship = {
 
 
-//     stopMachine() {
-//         this.stop();
-//         console.log(`${this.name} lifting anchor down`);
+    // const vehicle = {
+    //     name: 'Argo',
+    //     move() {
+    //         console.log(`${this.name} is moving`);
+    //     },
+    //     stop() {
+    //         console.log(`${this.name} stopped`);
+    //     },
+    // }
 
-//     },
-//     startMachine() {
+    // // console.log(vehicle);
 
-//         console.log(`${this.name} lifting anchor up`);
-//         this.move();
-//     },
-//     __proto__: vehicle,
-// };
+    // const ship = {
 
-// getOwnProps(ship);
+
+    //     stopMachine() {
+    //         this.stop();
+    //         console.log(`${this.name} lifting anchor down`);
+
+    //     },
+    //     startMachine() {
+
+    //         console.log(`${this.name} lifting anchor up`);
+    //         this.move();
+    //     },
+    //     __proto__: vehicle,
+    // };
+
+    // getOwnProps(ship);
