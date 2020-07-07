8a9e2688933e6e2ba94bd683763801af62e99823
Nadyazns <nadya_zns@yahoo.com>
2020-07-07 16:59:24 +0300

    ok

---
+++ lesson19/task3/index.js
@@ -0,0 +1,28 @@
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+};
+
+User.prototype.sayHi = function() {
+    console.log(`Hi, I am ${this.name}`);
+};
+User.prototype.requestNewPhoto = function() {
+    console.log(`New photo request was sent for ${this.name}`);
+};
+User.prototype.setAge = function() {
+
+    if (this.age > 0) {
+        if (this.age >= 25) {
+            return this.requestNewPhoto();
+        }
+        return this.age;
+    } else {
+        return false;
+    }
+};
+
+
+// const user1 = new User('Tom', -1);
+// const user2 = new User('Nadya', 33);
+
+// console.log(user1.setAge());
