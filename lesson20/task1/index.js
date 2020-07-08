05c0faa66aa4bea1c86c197ba494ce9bd392d1fe
Nadyazns <nadya_zns@yahoo.com>
2020-07-08 11:14:27 +0300

    ok

--- lesson20/task1/index.js
+++ lesson20/task1/index.js
@@ -1,78 +1,36 @@
-// class User {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
-
-//     sayHi() {
-//         console.log(`Hi, I am ${this.name}`);
-//     }
-//     requestNewPhoto() {
-//         console.log(`New photo request was sent for ${this.name}`);
-//     }
-//     setAge(a) {
-//         if (a < 0) {
-//             return false;
-//         }
-//         this.age = a;
-//         if (a >= 25) {
-//             this.requestNewPhoto();
-//         }
-//         return this.age;
-//     }
-//     static createEmpty() {
-
-//         this.name = " ";
-//         this.age = null;
-
-//     }
-// }
-
-
-
-// const user1 = new User('Tom', -1);
-// const user2 = new User('Nadya', 33);
-// console.log(User.createEmpty());
-
-// console.log(user1.setAge());
-
-class Vehicle {
-    constructor(name, numberOfWheels) {
-        this.numberOfWheels = numberOfWheels;
+class User {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
     }
 
-    move() {
-        console.log(`${this.name} is moving`);
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
     }
-    stop() {
-        console.log(`${this.name} stopped`);
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
     }
-};
+    setAge(a) {
+        if (a < 0) {
+            return false;
+        }
+        this.age = a;
+        if (a >= 25) {
+            this.requestNewPhoto();
+        }
+        return this.age;
+    }
+    static createEmpty() {
 
-// console.log(vehicle);
-
-class Ship extends Vehicle {
-    constructor(name, maxSpeed) {
-        super(name, false);
-        this.maxSpeed = maxSpeed;
+        return new User(' ', null);
 
     }
+}
 
-    stopMachine() {
-        this.stop();
-        console.log(`${this.name} lifting anchor down`);
 
-    }
-    startMachine() {
 
-        console.log(`${this.name} lifting anchor up`);
-        this.move();
-    }
+const user1 = new User('Tom', -1);
+const user2 = new User('Nadya', 33);
+console.log(User.createEmpty());
 
-};
-
-const ship1 = new Ship('Aurora', 100);
-console.log(ship1);
-
-ship1.stopMachine();
+console.log(user1.setAge());
