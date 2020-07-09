5ee89de907aebb4ffb1103c56b8a5fabf790b9bc
Nadyazns <nadya_zns@yahoo.com>
2020-07-09 17:39:16 +0300

    Order

--- lesson20/task4/index.js
+++ lesson20/task4/index.js
@@ -1,22 +1,22 @@
 export class Order {
     constructor(price, city, type) {
-        this._id = `${Math.random()}`;
-        this._price = price;
-        this._dateCreated = new Date;
-        this._isConfirmed = false;
-        this._dateConfirmed = null;
-        this._city = city;
-        this._type = type;
+        this.id = `${Math.random()}`;
+        this.price = price;
+        this.dateCreated = new Date;
+        this.isConfirmed = false;
+        this.dateConfirmed = null;
+        this.city = city;
+        this.type = type;
     }
     checkPrice() {
-        return Boolean(this._price > 1000);
+        return Boolean(this.price > 1000);
     }
     confirmOrder() {
-        this._isConfirmed = true;
-        this._dateConfirmed = new Date;
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date;
     }
     isValidType() {
-        if (this._type === 'Buy' || this._type === 'Sell') return true;
+        if (this.type === 'Buy' || this.type === 'Sell') return true;
         else return false;
     }
 }
