10baf6d2eb8bc1f8d163248cfa2b172bc0b5d998
Nadyazns <nadya_zns@yahoo.com>
2020-07-09 14:41:53 +0300

    walletclass

---
+++ lesson20/task3/index.js
@@ -0,0 +1,28 @@
+export class Wallet {
+    // constructor(){
+    //     this._balance=0;
+    // }
+    _balance = 0;
+
+    getBalance() {
+        return this._balance;
+    }
+    deposit(amount) {
+        this._balance += amount;
+    }
+    withdraw(amount) {
+        if (amount > this._balance) {
+            console.log('No enough funds');
+            return;
+        }
+        this._balance -= amount;
+    }
+}
+
+const wallet1 = new Wallet();
+
+
+console.log(wallet1.getBalance());
+wallet1.deposit(70);
+console.log(wallet1.getBalance());
+wallet1.withdraw(80);
