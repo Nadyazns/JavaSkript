fcb0f3fb13815c1e2f1da124d2c19b45ad6149bd
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 16:26:31 +0300

    createbutton

--- lesson21/task8/index.js
+++ lesson21/task8/index.js
@@ -2,9 +2,10 @@
      // document.querySelector('body')
      //     .append(document.createElement('button')
      //         .textContent = buttonText);
-     const body = document.querySelector('body');
-     const button = document.createElement('button');
-     return body.append(button.textContent = buttonText);
+     const body = document.querySelector("body");
+     const button = document.createElement("button");
+     button.textContent = buttonText;
+     return body.append(button);
  }
 
  //  console.log(createButton('hey'));
