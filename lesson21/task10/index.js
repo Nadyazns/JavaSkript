fa225c7acf88f12e0429b8078f416e12e3c84dc6
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 18:26:04 +0300

    https://github.com/Nadyazns/JavaSkript/tree/master/lesson21/task10

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,10 +1,11 @@
-export function finishForm() {
-    const doc = document.querySelector(".login-form");
-    const newInput = document.createElement("input");
-    newInput.setAttribute("type", "password");
-    newInput.setAttribute("name", "login");
-    doc.append(newInput);
+ export function finishForm() {
+     const doc = document.querySelector(".login-form");
+     document.querySelector("input").setAttribute("name", "login");
+     const newInput = document.createElement("input");
+     newInput.setAttribute("type", "password");
+     newInput.setAttribute("name", "password");
+     doc.append(newInput);
 
-}
+ }
 
-// finishForm();
+ finishForm();
