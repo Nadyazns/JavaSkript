b497b129c1b4c2d8be9fbebdf3dbae24251395ef
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 13:51:07 +0300

    innerOfElems

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -2,20 +2,20 @@ export function getTitle() {
 
     return document.querySelector('.title').textContent;
 }
-console.log(getTitle());
+// console.log(getTitle());
 
 export function getDescription() {
     return document.querySelector('.about').innerText;
 }
-console.log(getDescription());
+// console.log(getDescription());
 
 export function getPlans() {
     return document.querySelector('.plans').innerHTML;
 }
-console.log(getPlans());
+// console.log(getPlans());
 
 export function getGoal() {
     return document.querySelector('.goal').outerHTML;
 }
 
-console.log(getGoal());
+// console.log(getGoal());
