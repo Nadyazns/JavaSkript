2f934dcf88ce8343547a582b657979afcdb1ceed
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 18:04:12 +0300

    attributes

---
+++ lesson21/task10/index.js
@@ -0,0 +1,10 @@
+export function finishForm() {
+    const doc = document.querySelector(".login-form");
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "password");
+    newInput.setAttribute("name", "login");
+    doc.append(newInput);
+
+}
+
+finishForm();
