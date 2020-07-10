5090b0af4c6308763ef3f4636f950a602274e4ee
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 17:23:25 +0300

    appendprepend

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -1,11 +1,16 @@
-export function finishList() {
-    const elem1 = document.createElement('li');
-    elem1.textContent = "1";
-    const elem2 = document.createElement('li');
-    elem2.textContent = "8";
+ export function finishList() {
+     const elem1 = document.createElement('li');
+     elem1.textContent = "1";
+     const elem4 = document.createElement('li');
+     elem4.textContent = "4";
+     const elem6 = document.createElement('li');
+     elem6.textContent = "6";
+     const elem8 = document.createElement('li');
+     elem8.textContent = "8";
+     document.querySelector('ul').prepend(elem1);
+     document.querySelector('.special').before(elem4);
+     document.querySelector('.special').after(elem6);
+     document.querySelector('ul').append(elem8);
 
-    document.querySelector('ul').prepend(elem1);
-    document.querySelector('ul').append(elem2);
-
-}
-// finishList();
+ }
+ finishList();
