3fc15e12e3d12da637f0be89b00636c534a580c3
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 21:00:10 +0300

    getPArent

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,6 +1,6 @@
 export function getSection(num) {
     const span = document.querySelector(`span[data-number='${num}']`);
     const div = span.closest('.box');
-    return console.log(div.dataset.section);
+    return div.dataset.section;
 }
 // getSection(6);
