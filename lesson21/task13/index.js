b95d224a19f26730756d3d948a3b618ab8b5fb1c
Nadyazns <nadya_zns@yahoo.com>
2020-07-10 20:56:50 +0300

    getparentofelems

---
+++ lesson21/task13/index.js
@@ -0,0 +1,6 @@
+function getSection(num) {
+    const span = document.querySelector(`span[data-number='${num}']`);
+    const div = span.closest('.box');
+    return console.log(div.dataset.section);
+}
+getSection(6);
