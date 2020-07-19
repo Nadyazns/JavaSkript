c7bfb6a02e99b34a668c2a1793c4e62d2a48278d
Nadyazns <nadya_zns@yahoo.com>
2020-07-19 12:35:10 +0300

    getTheDateDiff

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,5 +1,26 @@
+// const startDate = new Date(2020, 0, 1, 20, 3);
+// const endDate = new Date(2020, 9, 6, 2, 20);
+
+
 function getDiff(startDate, endDate) {
+
+    const diffInMs = Math.abs(startDate.getTime() - endDate.getTime());
+
+    const seconds = Math.floor((diffInMs / 1000) % 60);
+
+    const minutes = Math.floor((diffInMs / 1000 / 60) % 60);
+    const hours = Math.floor((diffInMs / 60 / 60) % 24);
+    const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    // const days = parseInt(startDate.getDate()) - parseInt(endDate.getDate());
+    // const hours = parseInt((startDate.getHours()) - parseInt(endDate.getHours()) % 24);
+    // const minutes = parseInt((startDate.getMinutes()) - parseInt(endDate.getMinutes()) % 60);
+    // const seconds = parseInt((startDate.getSeconds()) - parseInt(endDate.getSeconds()) % 60);
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
 }
 
-getDiff(new Date())
+// console.log(getDiff(startDate, endDate));
+export { getDiff }
+
+// algo
+// 1 startDate - endDate, math.abs
