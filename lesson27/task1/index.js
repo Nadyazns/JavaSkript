99df5299f8b3ec6248c1a4556c7e0e2020aadd15
Nadyazns <nadya_zns@yahoo.com>
2020-07-23 13:48:09 +0300

    getLocalStorageDataJSON

---
+++ lesson27/task1/index.js
@@ -0,0 +1,23 @@
+// localStorage.clear();
+// localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
+// localStorage.setItem('name', 'Tom');
+// localStorage.setItem('age', JSON.stringify(17));
+// console.log(JSON.parse(localStorage.getItem('hobbies')));
+
+const getLocalStorageData = () => {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                const newValue = JSON.parse(value);
+            } catch (e) {
+                newValue = value;
+            }
+            return {
+                ...acc,
+                [key]: newValue,
+            };
+        }, {});
+};
+
+// console.log(getLocalStorageData());
