68cd3c450f595435ad8bcad9f3e8535fea392a94
Nadyazns <nadya_zns@yahoo.com>
2020-07-24 13:56:10 +0300

    ok

--- goose/index.js
+++
@@ -1,8 +0,0 @@
-function doublePower(currentPowers) {
-
-    for (let i = 0; i <= currentPowers.length; i++) {
-        currentPowers[i] = currentPowers[i] * 2;
-    }
-    return currentPowers;
-}
-console.log(doublePower([100, 200, 3]))
