c942638853dce4862d632e97e43efcbb9eebac79
Nadyazns <nadya_zns@yahoo.com>
2020-07-26 16:51:32 +0300

    recursion/рекурсия

---
+++ lesson28/task2/index.js
@@ -0,0 +1,36 @@
+// const fav = ['id-6', 'id-17', ];
+
+// const tree = {
+//     id: 'id-1',
+//     name: 'Products',
+//     nodes: [{
+//             id: 'id-2',
+//             name: 'Products',
+//             nodes: [{
+//                 id: 'id-6',
+//                 name: 'Products',
+//                 nodes: []
+//             }]
+//         },
+//         {
+//             id: 'id-17',
+//             name: 'Products',
+//             nodes: []
+//         }
+//     ],
+// };
+
+export const markFavorites = (tree, fav) => {
+    const isFav = fav.includes(tree.id);
+
+    return {
+        ...tree,
+        isFavorite,
+        nodes: tree.nodes.map(childNode => markFavorites(childNode, fav)),
+
+    }
+};
+
+const result = markFavorites(tree, fav);
+
+console.log(result);
