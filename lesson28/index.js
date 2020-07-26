6b7300a2b75db16e6c787bee85ffd427d4474da2
Nadyazns <nadya_zns@yahoo.com>
2020-07-26 16:27:55 +0300

    chaining

---
+++ lesson28/index.js
@@ -0,0 +1,28 @@
+const calc = initValue => {
+    let result = initValue;
+    const calculator = {
+        add(value) {
+            result += value;
+            return calculator;
+        },
+        mult(value) {
+            result *= value;
+            return calculator;
+        },
+        substract(value) {
+            result -= value;
+            return calculator;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result;
+        }
+    };
+    return calculator;
+};
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+console.log(result);
